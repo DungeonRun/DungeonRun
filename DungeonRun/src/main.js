1052706f4ab94adcd0f8137d8c40b6ab95f7eef1
Risuna <risunantimana4@gmail.com>
2025-09-20 14:46:23 +0200

    chore: initial commit with Vite setup

---
+++ DungeonRun/src/main.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+
+
+
+const scene = new THREE.Scene();
+
+
+
+const cubeGeometry = new THREE.BoxGeometry(1,1,1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color: "red"});
+
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry, 
+  cubeMaterial
+)
+
+scene.add(cubeMesh);
+
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30
+)
+
+
+camera.position.z = 5
+
+
+const canvas = document.querySelector('canvas.threejs')
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+
+
+
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+renderer.render(scene, camera)
